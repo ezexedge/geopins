380f0517247ee9f99032e07292438baa6eff026c
ezequiel <ezeedge@gmail.com>
2020-12-12 03:16:04 -0300

    guardando cambios

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {GoogleLogin} from 'react-google-login'
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+
+  const onSuccess = googleUser => {
+    console.log({googleUser})
+    const idToken = googleUser.getAuthResponse().id_token
+
+    console.log({idToken})
+
+  }
+
+  return <GoogleLogin clientId="866332901152-ujgmfshhnkro287am2r1tvhifcs9aius.apps.googleusercontent.com" onSuccess={onSuccess} />
 };
 
 const styles = {
