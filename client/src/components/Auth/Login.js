40231cfa558eb8127f57575c9f54641b5fcc2816
ezequiel <ezeedge@gmail.com>
2020-12-15 09:52:40 -0300

    finalizando auth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,15 +1,32 @@
 import React from "react";
+import {GraphQLClient} from 'graphql-request'
 import {GoogleLogin} from 'react-google-login'
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+{
+  me {
+  	_id
+    name
+    email
+    picture
+}
+}
+` 
+
 const Login = ({ classes }) => {
 
-  const onSuccess = googleUser => {
+  const onSuccess = async googleUser => {
     console.log({googleUser})
     const idToken = googleUser.getAuthResponse().id_token
+   const client =  new GraphQLClient('http://localhost:4000/graphql',{
+      headers : {authorization: idToken}
+    })
 
-    console.log({idToken})
+    const data = await client.request(ME_QUERY)
+
+    console.log({data})
 
   }
 
